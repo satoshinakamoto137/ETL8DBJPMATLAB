4f85dd60eb25ba77a693fa8519a25a262fdc3bab
聡中本１３７ <andes.autenticos@gmail.com>
2019-05-05 20:26:37 -0500

    Add files via upload
    Improved algorithm for read images from DB.

--- MAT_FUNCT/str2im4b.m
+++ MAT_FUNCT/str2im4b.m
@@ -3,18 +3,23 @@ function [ Im ] = str2im4b(c_arr)  %%the image must be char array!
 c_s = size(c_arr,2); %%must be 8128
 Vim = zeros(1, 16256);
 Im = zeros(127,128);
-saver = 100000000;
 
 for i=1:c_s
 
  tm = c_arr(1,i);
  btm = dec2bin(tm); 
- btmi = str2num(btm);
- btmu = saver + btmi;
- a = int64(btmu);             
- std = int2str(a);
- st1 = std(2:5);
- st2 = std(6:9);
+ btmin = fliplr(btm);
+ btmx = '';
+ z ='0';
+ if(size(btmin,2) <= 7)
+    btmin = strcat(btmin, '0000000');
+ end
+ for b=1:8
+    btmx = strcat(btmin(1,b), btmx);
+ end
+
+ st1 = btmx(1:4);
+ st2 = btmx(5:8);
  p1 = bin2dec(st1);
  p2 = bin2dec(st2);
  idou = 2 * i;
